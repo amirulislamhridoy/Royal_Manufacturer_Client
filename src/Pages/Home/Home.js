5abe13e1baacd113144872e70e65e68de8986674
Your Name <hossainhridoy@30687@gmail.com>
2022-08-01 15:02:06 +0600

    first commit

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from '../../Shared/Header/Header';
+
+const Home = () => {
+    return (
+        <main className='max-w-7xl mx-auto'>
+            <Header />
+        </main>
+    );
+};
+
+export default Home;
---
+++ src/Shared/Header/Header.js
@@ -0,0 +1,46 @@
+import React from "react";
+import {Link} from 'react-router-dom'
+import icon from '../../icons/logo.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+
+const Header = () => {
+ 
+  const btn = (
+    <>
+      <li><Link to='/'>Home</Link></li>
+    </>
+  );
+  return (
+    <nav>
+      <div className="navbar">
+        <div className="navbar-start">
+          <Link to='/' className="btn btn-ghost">
+            <img className="w-10" src={icon} alt="" />
+            <span><b>Royal</b> <br /> Manufacturer</span>
+          </Link>
+        </div>
+        <div className="navbar-end hidden lg:flex">
+          <ul className="menu menu-horizontal p-0">
+            {btn}
+          </ul>
+        </div>
+        <div className="navbar-end lg:hidden">
+        <div className="dropdown dropdown-end">
+            <label tabIndex="0" className="btn btn-ghost lg:hidden">
+              <FontAwesomeIcon className='h-5 w-5' icon={faBars} />
+            </label>
+            <ul
+              tabIndex="0"
+              className="menu menu-compact dropdown-content mt-3 mr-4 p-2 shadow-xl bg-base-100 w-48"
+            >{btn}
+            </ul>
+          </div>
+        </div>
+      </div>
+      
+    </nav>
+  );
+};
+
+export default Header;
