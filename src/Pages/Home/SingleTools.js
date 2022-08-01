a52de31f9bd9c2ce0b16c2dc48818686dfc1e920
Your Name <hossainhridoy@30687@gmail.com>
2022-08-01 19:44:53 +0600

    banner

--- src/Pages/Home/SingleTools.js
+++ src/Pages/Home/SingleTools.js
@@ -1,14 +1,19 @@
 import React from "react";
+import { useState } from "react";
 
 const SingleTools = ({tool}) => {
+    const [more, setMore] = useState(true)
     const {available, description, img, minium, name, price, _id} = tool
     
   return (
     <div className="card w-96 bg-base-100 shadow-xl mx-auto">
       <div className="card-body">
-        <img src={img} alt='Tools Photo' />
+        <img className='rounded-xl' src={img} alt='Tools Photo' />
         <h2 className="card-title">{name}</h2>
-        <p>{description}</p>
+
+        {more && <p>{description.length > 65 ? <div>{description.slice(0, 65)} <span onClick={() => setMore(!more)} className='cursor-pointer'>...more</span></div> : description}</p>}
+        {more || <p>{description} <span onClick={() => setMore(!more)} className='cursor-pointer'>...less</span></p>}
+
         <p>Minium Order: {minium}</p>
         <p>Available Tools: {available}</p>
         <h3 className="text-2xl font-bold">Price: {price}</h3>
